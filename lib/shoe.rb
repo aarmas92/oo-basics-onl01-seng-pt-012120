9f069c1b12b4b0b62c10d44fc953fe53fa5011e0
Alvaro Armas <aarmas92@outlook.com>
2020-02-06 22:37:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
-def initialize (brand)
-  @brand = brand
-end  
+  def initialize (brand)
+    @brand = brand
+  end  
   
 end  
